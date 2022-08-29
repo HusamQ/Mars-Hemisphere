80f9d3e7730923f6bc67dfbe0af0e07d0e7869c6
HusamQ <107529940+HusamQ@users.noreply.github.com>
2022-08-28 23:40:33 -0400

    updates to the juypyter notebook

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
+
 app = Flask(__name__)
 
 #Use flask_pymongo to set up mongo connection
-app.config["MONGO_URL"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app, static_url_path='/static')
 
 @app.route("/")
 def index():
